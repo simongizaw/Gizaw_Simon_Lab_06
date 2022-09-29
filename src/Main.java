08b11d1ced0912372698004dc4d54d9d4cccb573
simon <simongizaw@gmail.com>
2022-09-29 13:22:13 -0400

    simongizaw

---
+++ src/Main.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        double cc = 0;
+        double ff = 0;
+        String trash = "";
+        System.out.print("What is your temprature in C ");
+
+
+        if (in.hasNextDouble())
+        {
+            cc= in.nextDouble();
+            cc =
+        }
+    }
+}

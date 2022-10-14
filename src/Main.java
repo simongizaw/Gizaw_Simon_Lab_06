915061bb79cbc8a033745fdceebfa58d531f70d5
simon <simongizaw@gmail.com>
2022-10-14 14:39:07 -0400

    finished

--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,27 @@
 import java.util.Scanner;
-
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args)
+    {
         Scanner in = new Scanner(System.in);
-        double cc = 0;
-        double ff = 0;
-        String trash = "";
-        System.out.print("What is your temprature in C ");
+        int cel = 0;
+        double fahr = 0;
+        String trash = " ";
 
 
-        if (in.hasNextDouble())
-        {
-            cc= in.nextDouble();
-            cc =
-        }
+            System.out.print("enter the temp in C");
+            if (in.hasNextInt()){
+                cel = in.nextInt();
+                fahr = ( cel * 1.8) + 32;
+                System.out.println("The temp in F is " + fahr);
+
+            } else {
+                trash = in.nextLine();
+                System.out.println(trash + " is not a valid input");
+            }
+
+
+
+
+
     }
 }
